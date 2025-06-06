3d519a3076e9952a694129f99c8bf74f192aa1f7
Nehaa1108 <127198995+Nehaa1108@users.noreply.github.com>
2025-06-06 02:30:07 -0400

    adding file and content

--- arraymethod.js
+++ arraymethod.js
@@ -7,6 +7,11 @@ const personArray =
     age: 25,
     country: "USA",
   },
+  {
+   name:"person4",
+    age: 26,
+    country: "USA",
+  },
    {
     name:"person2",
     age: 30,
@@ -30,3 +35,53 @@ console.log(getAllNames)
 
 
 //find
+let getPersonFromUSA = personArray.find((singlePerson,index)=>
+{
+  return singlePerson.country === 'USA'
+})
+
+console.log(getPersonFromUSA)
+
+//filter
+let getAllPersonFromUSA = personArray.filter((singlePerson,index)=>
+{
+  return singlePerson.country === "USA"
+})
+
+console.log(getAllPersonFromUSA)
+
+
+//some--- boolean
+//some will return true when any condition  will satisfy 
+let checkSomeExample = personArray.some((singlePerson,index)=>
+{
+  return singlePerson.age >=25
+})
+
+console.log(checkSomeExample)
+
+//every----
+//every will return true when all the condition will satisfy 
+//all person name should be person1 then it will return true
+let checkEveryExample = personArray.every((singlePerson,index)=>
+{
+  return singlePerson.name ==="person1"
+})
+
+console.log(checkEveryExample)
+
+
+//include--
+//Determines whether an array includes a certain element, returning true or false as appropriate.
+const fruitArray = ['apple','banana','orange']
+console.log(fruitArray.includes('apple'), fruitArray.indexOf('banana'))
+//Returns the index of the first occurrence of a value in an array, or -1 if it is not present.
+
+//findIndex--
+//Returns the index of the first element in the array where predicate is true, and -1 otherwise.
+let getIndexOfPersonFromIndia = personArray.findIndex(singlePerson=>
+{
+  return  singlePerson.country === 'India'
+}
+)
+console.log(getIndexOfPersonFromIndia)
