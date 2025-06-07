1b829a2e43bd58c9bba70d18608f9b7142ea37e8
Nehaa1108 <127198995+Nehaa1108@users.noreply.github.com>
2025-06-06 23:50:28 -0400

    understanding styling

--- fundamental/fundamental/src/components/products/components/ProductItem.jsx
+++ fundamental/fundamental/src/components/products/components/ProductItem.jsx
@@ -1,15 +1,16 @@
-
+import style from '../components/style.css'
 
 function ButtonComponent()
 {
-  return <button>Click</button>
+  console.log("style",style)
+  return <button className={style.custombutton}>Click</button>
 }
 
 const ProductItem = ({singleProductItem,key})=>
 {
   return (
     <div key={key}>
-        <p>{singleProductItem}</p>
+        <p style={{color:"green"}}>{singleProductItem}</p>
         <ButtonComponent/>
         <h2>Product :- {singleProductItem}</h2>
     </div>
